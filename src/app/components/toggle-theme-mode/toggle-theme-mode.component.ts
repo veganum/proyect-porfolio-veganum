16764a1b8a745e34d9dba92f393f2ab28de22141
veganum <veganum@hotmail.com>
2024-01-04 23:55:26 +0100

    mejorando estilos y toogle

--- src/app/components/toggle-theme-mode/toggle-theme-mode.component.ts
+++ src/app/components/toggle-theme-mode/toggle-theme-mode.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faMoon, faSun } from '@fortawesome/free-solid-svg-icons';
+import { CommonModule } from '@angular/common';
 
 @Component({
   standalone: true,
-  imports: [],
+  imports: [FontAwesomeModule, CommonModule],
   selector: 'app-toggle-theme',
   templateUrl: './toggle-theme-mode.component.html',
-  styleUrls: ['./toggle-theme-mode.component.css'],
+  styleUrls: ['./toggle-theme-mode.component.scss'],
 })
 export class ToggleThemeModeComponent {
+  faMoon = faMoon;
+  faSun = faSun;
   darkThemeEnabled: boolean = false;
 
   constructor() {
