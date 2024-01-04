dd89b81081a91b533f62b58e550db8603b7f1ead
veganum <veganum@hotmail.com>
2024-01-04 21:41:22 +0100

    toogle component

---
+++ src/app/components/toggle-theme-mode/toggle-theme-mode.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-toggle-theme-mode',
+  templateUrl: './toggle-theme-mode.component.html',
+  styleUrls: ['./toggle-theme-mode.component.css'],
+})
+export class ToggleThemeModeComponent {
+  constructor() {}
+}
