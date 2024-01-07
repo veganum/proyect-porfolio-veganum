e6cb9805abd83c66b5e5b0ade5e4b3fb22e8e964
veganum <veganum@hotmail.com>
2024-01-07 22:36:59 +0100

    mejoras home

--- src/app/components/toggle-theme-mode/toggle-theme-mode.component.ts
+++ src/app/components/toggle-theme-mode/toggle-theme-mode.component.ts
@@ -1,18 +1,14 @@
 import { Component } from '@angular/core';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { faMoon, faSun } from '@fortawesome/free-solid-svg-icons';
 import { CommonModule } from '@angular/common';
 
 @Component({
   standalone: true,
-  imports: [FontAwesomeModule, CommonModule],
+  imports: [CommonModule],
   selector: 'app-toggle-theme',
   templateUrl: './toggle-theme-mode.component.html',
   styleUrls: ['./toggle-theme-mode.component.scss'],
 })
 export class ToggleThemeModeComponent {
-  faMoon = faMoon;
-  faSun = faSun;
   darkThemeEnabled: boolean = false;
 
   constructor() {
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,23 @@ module.exports = {
   darkMode: 'class',
   content: ['./src/**/*.{html,ts}'],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        text: 'text 5s ease infinite',
+      },
+      keyframes: {
+        text: {
+          '0%, 100%': {
+            'background-size': '200% 200%',
+            'background-position': 'left center',
+          },
+          '50%': {
+            'background-size': '200% 200%',
+            'background-position': 'right center',
+          },
+        },
+      },
+    },
   },
   plugins: [require('autoprefixer')],
 };
