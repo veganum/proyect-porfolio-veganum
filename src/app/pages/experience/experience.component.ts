55f57497532f34f8b6a1f60ad006028bfe9e4e9a
veganum <veganum@hotmail.com>
2024-01-08 22:29:32 +0100

    expirience component

--- src/app/pages/experience/experience.component.ts
+++ src/app/pages/experience/experience.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
 import { LoadingComponent } from '../../components/loading/loading.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   standalone: true,
-  imports: [LoadingComponent],
+  imports: [LoadingComponent, CommonModule],
   selector: 'app-experience',
   templateUrl: './experience.component.html',
   styleUrls: ['./experience.component.scss'],
 })
 export class ExperienceComponent {
+  tabIndex: string = 'Trabajo';
+
   constructor() {}
+
+  openCity(event: any) {
+    this.tabIndex = event;
+  }
 }
