d3e1fee75b097d5b8b7938a3b8f7cb75517b3c4f
veganum <veganum@hotmail.com>
2024-01-11 21:43:17 +0100

    Social-pill y definicion de pagina

---
+++ src/app/components/social-pill/social-pill.component.ts
@@ -0,0 +1,35 @@
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
+@Component({
+  selector: 'app-social-pill',
+  standalone: true,
+  imports: [CommonModule, FontAwesomeModule],
+  template: `<a
+    class="
+  bg-white/5
+    border dark:border-white border-teal-600 rounded-full
+    flex justify-center items-center gap-x-2
+    py-1 px-2 md:py-2 md:px-4
+    text-xs md:text-base
+    text-teal-600 dark:text-white
+    transition
+    hover:scale-110 hover:bg-white/10
+    shadow-lg shadow-cyan-500/50
+    m-1.5
+  "
+    [href]="link"
+    target="_blank"
+    rel="noopener noreferrer">
+    <fa-icon [icon]="icon"></fa-icon>
+    {{ nameSocial }}
+  </a>`,
+  styleUrl: './social-pill.component.css',
+})
+export class SocialPillComponent {
+  @Input() nameSocial: string | undefined;
+  @Input() link: string | undefined;
+
+  @Input() icon: any;
+}
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,16 +1,21 @@
 import { Component } from '@angular/core';
 import { NgOptimizedImage } from '@angular/common';
 import { LoadingComponent } from '../../components/loading/loading.component';
+import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faEnvelope, faIdCard } from '@fortawesome/free-solid-svg-icons';
+import { SocialPillComponent } from '../../components/social-pill/social-pill.component';
 
 @Component({
   standalone: true,
-  imports: [NgOptimizedImage, LoadingComponent],
+  imports: [NgOptimizedImage, LoadingComponent, SocialPillComponent],
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent {
-  constructor() {}
-
   heroImg = '../../../assets/img/foto-carnet.webp';
+  linkedin = faLinkedin;
+  github = faGithub;
+  mail = faEnvelope;
+  curriculum = faIdCard;
 }
